0ad3293b6fea94aff55acb32c83d1f1da049c283
Alex Curran <alexcurran.va@gmail.com>
2021-10-25 10:07:54 -0500

    Add map-the-debris algo

---
+++ intermediate/map-the-debris.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * @param {object[]} arr 
+ * @returns {object[]}
+ */
+function orbitalPeriod(arr) {
+    return arr.map((obj) => {
+        return {
+            name: obj.name,
+            orbitalPeriod: calcOrbitalPeriod(obj.avgAlt)
+        };
+    });
+}
+
+/**
+ * @param {number} avgAlt
+ * @returns {number}
+ */
+function calcOrbitalPeriod(avgAlt) {
+    const GM = 398600.4418;
+
+    const earthRadius = 6367.4447;
+    const semiMajorAxis = earthRadius + avgAlt;
+
+    return Math.round( (2 * Math.PI) * Math.sqrt(semiMajorAxis ** 3 / GM) );
+}
+
+console.log( orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]) ); // [{name: "sputnik", orbitalPeriod: 86400}]
+console.log( orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) ); // [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}]
