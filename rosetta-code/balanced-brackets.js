2fc5040f0d2b94150278ddbfa66c7fb6a2a79020
Alex Curran <alexcurran.va@gmail.com>
2022-02-08 09:42:27 -0600

    Working solution for balanced-brackets.js

---
+++ rosetta-code/balanced-brackets.js
@@ -0,0 +1,35 @@
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {boolean}
+ */
+function isBalanced(str) {
+    const bracketMap = new Map([
+        ["[", "]"]
+    ]);
+
+    let stack = [];
+
+    // If the bracket is an opening bracket, then push it on to the stack
+    for (let bracket of str) {
+        if (bracketMap.get(bracket)) {
+            stack.push(bracket);
+        } else {
+            const poppedBracket = stack.pop();
+
+            // Check to see if the popped bracket is the matching bracket
+            if (bracketMap.get(poppedBracket) !== bracket) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log( isBalanced("[[]]") ); // true
+console.log( isBalanced("[]]]") ); // false
