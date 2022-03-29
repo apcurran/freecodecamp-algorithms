6b03627004fa879f24a2271f178adee26d281fad
Alex Curran <alexcurran.va@gmail.com>
2022-03-29 11:41:05 -0500

    Working binary search solution

---
+++ coding-interview-prep/bin-search.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(log n)
+ * space: O(log n)
+ * 
+ * @param {number[]} searchList 
+ * @param {number} value 
+ * @returns {number[]|string}
+ */
+function binarySearch(searchList, value) {
+    let start = 0;
+    let end = searchList.length - 1;
+    let resArr = [];
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        const currArrVal = searchList[middle];
+
+        resArr.push(currArrVal);
+
+        if (currArrVal === value) {
+            return resArr;
+        }
+
+        if (value < searchList[middle]) {
+            // search left half
+            end = middle - 1;
+        } else {
+            // search right half
+            start = middle + 1;
+        }
+    }
+
+    return "Value Not Found";
+}
+
+const testArray = [
+    0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 49, 70
+];
+
+console.log( binarySearch(testArray, 0) ); // [13, 5, 2, 0]
+console.log( binarySearch(testArray, 1) ); // [13, 5, 2, 0, 1]
