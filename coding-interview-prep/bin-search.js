aa583c7f46c4349a7063543fa87f8acc3afde0da
Alex Curran <alexcurran.va@gmail.com>
2022-03-29 12:16:38 -0500

    Improved version that prevents overflows

--- coding-interview-prep/bin-search.js
+++ coding-interview-prep/bin-search.js
@@ -1,7 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(log n)
+//  * space: O(log n)
+//  * 
+//  * @param {number[]} searchList 
+//  * @param {number} value 
+//  * @returns {number[]|string}
+//  */
+// function binarySearch(searchList, value) {
+//     let start = 0;
+//     let end = searchList.length - 1;
+//     let resArr = [];
+
+//     while (start <= end) {
+//         // to prevent overflow...
+//         let middle = start + Math.floor((end - start) / 2);
+//         const currArrVal = searchList[middle];
+
+//         resArr.push(currArrVal);
+
+//         if (currArrVal === value) {
+//             return resArr;
+//         }
+
+//         if (value < searchList[middle]) {
+//             // search left half
+//             end = middle - 1;
+//         } else {
+//             // search right half
+//             start = middle + 1;
+//         }
+//     }
+
+//     return "Value Not Found";
+// }
+
 /**
- * solution 1
+ * solution 2 - slightly faster calculation of middle
  * time: O(log n)
  * space: O(log n)
  * 
@@ -15,7 +52,9 @@ function binarySearch(searchList, value) {
     let resArr = [];
 
     while (start <= end) {
-        let middle = Math.floor((start + end) / 2);
+        // to prevent overflow...
+        // slightly faster unsigned right shift operator
+        let middle = (start + end) >>> 1;
         const currArrVal = searchList[middle];
 
         resArr.push(currArrVal);
