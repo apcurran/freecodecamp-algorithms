baddad99a4f7352c934b93fbd4277ac131ceebf0
Alex Curran <alexcurran.va@gmail.com>
2024-04-02 14:03:43 -0500

    Refactor with Math.min() and Math.max() instead of array.sort()

--- intermediate/sum-all-nums-in-a-range.js
+++ intermediate/sum-all-nums-in-a-range.js
@@ -9,17 +9,18 @@
  * @returns {number} 
  */
 function sumAll(arr) {
-    const [firstNum, secondNum] = arr.sort((a, b) => a - b);
-    let total = 0;
+    const startNum = Math.min(arr[0], arr[1]);
+    const endNum = Math.max(arr[0], arr[1]);
+    let sum = 0;
 
-    for (let i = firstNum; i <= secondNum; i++) {
-        total += i;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
     }
 
-    return total;
+    return sum;
 }
 
-console.log( sumAll([5, 10]) );
+console.log( sumAll([5, 10]) ); // 45
 
 /**
  * Solution 2
@@ -34,4 +35,4 @@ function sumAllArithmeticProgression(arr) {
     return (numCount * (startNum + endNum)) / 2;
 }
 
-console.log( sumAllArithmeticProgression([5, 10]) );
+// console.log( sumAllArithmeticProgression([5, 10]) );
